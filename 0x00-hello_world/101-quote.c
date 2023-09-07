e43ef98c46744c4efeb4961985caa45546a632bf
Emyjakarta <ukonud4@gmail.com>
2023-09-07 12:03:13 -0700

    Unix

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -8,7 +8,7 @@
  */
 int main(void)
 {
-	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n"
+	char n[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
 	write(2, n, strlen(n));
 	return (1);
