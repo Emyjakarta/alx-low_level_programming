07b11714f4af60fdfcb54bd65ae5b0769b914866
Emyjakarta <ukonud4@gmail.com>
2023-09-07 06:13:36 -0700

    First C Program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,10 @@
 #include <stdio.h>
 
 /** 
- * main - A C program that prints prints exactly "Programming is like building a multilingual puzzle"
+ * main - Entry point 
+ * Description: A C program that prints prints exactly "Programming is like building a multilingual puzzle"
  * Return : Always 0 (Success)
  */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
