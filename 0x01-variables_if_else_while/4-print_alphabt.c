a29ebbb7f7a788cf02ef2f869a6144a2d11a4b77
Emyjakarta <ukonud4@gmail.com>
2023-09-08 09:06:55 -0700

    eliminate two letters and print

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-Print all the letters of the alphabets
+ * except q and e in lowercase using putchar
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int P = 97;
+
+	for (P <= 100; P <= 112; P <= 122)
+	putchar(P);
+	putchar('\n');
+	return (0);
+}
