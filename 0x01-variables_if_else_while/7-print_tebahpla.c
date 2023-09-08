3293e31338f52ca563f1b265e715ac1bac0a978c
Emyjakarta <ukonud4@gmail.com>
2023-09-08 10:36:39 -0700

    letters in reverse using putchar

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main-print lower case letters in reverse
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int R;
+
+	for (R = 122; R >= 97; P--)
+	putchar(R);
+	putchar("\n");
+	return (0);
+}
+
