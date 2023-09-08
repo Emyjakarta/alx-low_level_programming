4118148b551cadd6a5adf9e9111731cdc9ad83e5
Emyjakarta <ukonud4@gmail.com>
2023-09-08 06:38:27 -0700

    Last digit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -11,11 +11,12 @@
 int main(void)
 {
 	int n;
+	int B;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
+	B = n % 10;
 	/* your code goes there */
-	int B = n % 10;
 	const char *C = "is";
 	const char *D = "and is greater than 5";
 	const char *E = "and is 0";
