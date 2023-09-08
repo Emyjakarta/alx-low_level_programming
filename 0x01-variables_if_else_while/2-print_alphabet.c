338ecbf5ffea3152ef101d41d97c4cb2f07323db
Emyjakarta <ukonud4@gmail.com>
2023-09-08 08:13:02 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main-Entry point
+ * use putchar to print the
+ * letters of the alphabet
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int P;
+
+	for (P = 97; P <= 122; P++)
+	putchar(P);
+	putchar("\n");
+	return (0);
+}
