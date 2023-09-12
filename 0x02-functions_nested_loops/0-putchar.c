ecdebf980332d6f0c8c1b1469ba1be9ab00fb323
Emyjakarta <ukonud4@gmail.com>
2023-09-12 02:38:14 -0700

    Print _putchar without using standard library

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,11 @@
+#include <main.h>
+/**
+ * main-Write a program that prints _putchar, followed by a new line.
+ * The program should return 0
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	_putchar("_putchar\n");
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/main.h
@@ -0,0 +1,1 @@
+char _putchar(char *Q1, ...);
