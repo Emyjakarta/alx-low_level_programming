b9a2ed2206a44bb36315500ff3ced58e0a7c2051
Emyjakarta <ukonud4@gmail.com>
2023-09-12 05:19:37 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_alphabet(void)-print alphabet in lowercase
+ * followed by a new line
+ * Return:Always 0 (Success)
+ */
+void print_alphabet(void)
+{
+	int alphabet;
+
+	for (alphabet = a; alphabet <= z, alphabet++)
+	_putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -2,5 +2,6 @@
 #define MAIN_H
 
 int _putchar(char Q);
+void print_alphabet(void);
 
 #endif
