0d484d75845e831bc7635c6201650a1744c3598a
Emyjakarta <ukonud4@gmail.com>
2023-09-12 06:30:18 -0700

    10x alphabet

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -8,8 +8,12 @@
 void print_alphabet_x10(void)
 {
 	int Q;
+	char alphabet;
 
 	for (Q = 1; Q <= 10; Q++)
-		_putchar(print_alphabet);
+	{	
+		for (alphabet = 'a'; alphabet <= 'z'; alphabet++)
+		_putchar(alphabet);
+	}
 	_putchar('\n');
 }
