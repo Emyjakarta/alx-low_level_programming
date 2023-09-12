756c4dbea723a2fcec1fcf360109a53920f21678
Emyjakarta <ukonud4@gmail.com>
2023-09-12 05:33:31 -0700

    alphabet

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -9,7 +9,7 @@ void print_alphabet(void)
 {
 	int alphabet;
 
-	for (alphabet = a; alphabet <= z, alphabet++)
+	for (alphabet = 'a'; alphabet <= 'z', alphabet++)
 	_putchar(alphabet);
 	putchar('\n');
 	return (0);
