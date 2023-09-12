91b66ebf0fca1ed3192f4a35437463c844b1f4ed
Emyjakarta <ukonud4@gmail.com>
2023-09-12 07:39:27 -0700

    lowercase or uppercase

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,8 +2,8 @@
 /**
  * _isalpha(int c)-a function that checks for alphabetic character.
  * @c: character to be checked
- * Returns 1 if c is a letter, lowercase or uppercase
- * Returns 0 otherwise
+ * Return:1 if c is a letter, lowercase or uppercase
+ * Return:0 otherwise
  */
 int _isalpha(int c)
 {
