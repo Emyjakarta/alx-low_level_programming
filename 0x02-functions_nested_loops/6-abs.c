fda52596b925fe87813ed6180f6f7962b1e8f41b
Emyjakarta <ukonud4@gmail.com>
2023-09-12 08:11:18 -0700

    Absolute value

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ *  int _abs(int)-a function that computes the
+ *  absolute value of an integer.
+ *  @Q: integer to be checked
+ *  Return:void
+ */
+int _abs(int Q)
+{
+	if (Q < 0 || Q = 0 || Q > 0)
+		putchar(Q);
+}
