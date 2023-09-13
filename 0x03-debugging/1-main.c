9b654d51d7b1caa56b8e40088932d40d5635f573
Emyjakarta <ukonud4@gmail.com>
2023-09-13 05:11:29 -0700

    Infinite loop

--- 0x03-debugging/1-main.c
+++ 0x03-debugging/1-main.c
@@ -12,12 +12,12 @@ int main(void)
 	printf("Infinite loop incoming :(\n");
 
 	i = 0;
-
-	while (i < 10)	/*The condition given for i will always be true*/
-	{
+	/**
+	*while (i < 10)	The condition given for i will always be true
+	{ 		Hence, infinite loop
 		putchar(i);
 	}
-
+	*/
 	printf("Infinite loop avoided! \\o/\n");
 	return (0);
 }
