fb3e5658d7a886ec9e0fdf9ca5cf2fc9d581f545
Emyjakarta <ukonud4@gmail.com>
2023-09-14 14:11:01 -0700

    Fizz Buzz

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -5,24 +5,25 @@
  * Fizz instead of the number and for the multiples of five
  * print Buzz. For numbers which are multiples of both three
  * and five print FizzBuzz.
- * @Q: variable that contains numbers 1 to 100
  * Return:Always 0 (Success)
  */
-int main(int Q)
+int main(void)
 {
+	unsigned long Q;
+
 	for (Q = 1; Q <= 100; Q++)
 	{
-		if ((Q % 3) == 0)
+		if ((Q % 15) == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if ((Q % 3) == 0)
 		{
 			printf("Fizz ");
 		}
 		else if ((Q % 5) == 0)
 		{
 			printf("Buzz ");
-		}
-		else if ((Q % 15) == 0)
-		{
-			printf("FizzBuzz ");
 		}
 		else
 			printf("%lu ", Q);
