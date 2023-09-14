bd47dde4295e704fb8fd2c746c7c613cd3bc1e1f
Emyjakarta <ukonud4@gmail.com>
2023-09-13 17:08:06 -0700

    fibonacci even

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,38 @@
+#include <stdio.h>
+/**
+ * main-a program that finds and prints
+ * the sum of the even-valued terms of
+ * a fibonacci sequence
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	unsigned long Q, R, S, T;
+
+	Q = 0;
+	R = 1;
+	unsigned long Total = 0;
+
+	for (i = 0; i < 33; i++)
+	{
+
+		S = Q + R;
+		Q = R;
+		R = S;
+
+		T = S % 2;
+		if (T == 0)
+		{
+			Total += S;
+		}
+		if (i == 32)
+		{
+			printf("\n");
+		}
+
+	}
+
+	printf("%lu\n", Total);
+	return (0);
+}
