19654f9331d761df8501068f4a3c5774c5e09420
Emyjakarta <ukonud4@gmail.com>
2023-09-18 10:48:16 -0700

    Every other character in a string

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,7 +1,7 @@
 #include "main.h"
 /**
- * puts2-a function that prints every other character 
- * of a string, starting with the first character, 
+ * puts2-a function that prints every other character
+ * of a string, starting with the first character,
  * followed by a new line.
  * @str: string variable.
  * Return:void
@@ -15,5 +15,5 @@ void puts2(char *str)
 		_putchar(str[Q]);
 	}
 	_putchar('\n');
-	
+
 }
