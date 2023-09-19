f4c23917c114b727c3a2ffe4a6bea9a779c958b1
Emyjakarta <ukonud4@gmail.com>
2023-09-19 05:31:12 -0700

    Random Password

---
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main-program that generates random valid passwords
+ * for the program 101-crackme
+ * Return:Always 0 (Success)
+ */
+int main(void)
+{
+	int password[64], Q, sum = 0, R;
+
+	srand(time(NULL));
+	for (Q = 0; Q < 64; Q++)
+	{
+		password[Q] = rand() % 78;
+		sum += password[Q] + '0';
+		_putchar(password[Q] + '0');
+		if ((2772 - sum) - '0' < 78)
+		{
+			R = 2772 - sum - '0';
+			sum += R;
+			_putchar(R + '0');
+			break;
+		}
+	}
+	return (0);
+}
