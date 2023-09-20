efedeb9da5349a57bb30a1234465fea1ad5a9f80
Emyjakarta <ukonud4@gmail.com>
2023-09-20 21:30:05 +0100

    Concatenate with restriction

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -13,30 +13,21 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int Q;
 	int R = 0;
-	int length = 0;
 
-	while (src[length] != '\0')
-	{
-		length++;
-	}
-	for (Q = 0; n > Q, dest[Q] != '\0'; Q++)
+	for (Q = 0; dest[Q] != '\0'; Q++)
 	{
 	}
 	while (src[R] != '\0')
 	{
-		if (n < length)
-		{
-			dest[Q] = src[R] - n;
-			Q++;
-			R++;
+		if(src[R] == n)
+		{	
+			break;
 		}
-		else if (n > length)
-		{
 			dest[Q] = src[R];
 			Q++;
 			R++;
-		}
-		dest[Q] = '\0';
-		return (dest);
+		
 	}
+	dest[Q] = '\0';
+	return (dest);
 }
