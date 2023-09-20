1a2407557727d07c93c408652c7f8fe77f0fa752
Emyjakarta <ukonud4@gmail.com>
2023-09-20 12:04:18 -0700

    concatenate with restrictions

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -2,7 +2,7 @@
 /**
  * _strncat-a function that concatenates two strings.
  * @dest: destination string
- * @src: source string 
+ * @src: source string
  * @n: maximum bytes to be used
  * The _strncat function is similar to the _strcat function, except that
  * it will use at most n bytes from src; and
@@ -11,5 +11,32 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-	
+	int Q;
+	int R = 0;
+	int length = 0;
+
+	while (src[length] != '\0')
+	{
+		length++;
+	}
+	for (Q = 0; n > Q, dest[Q] != '\0'; Q++)
+	{
+	}
+	while (src[R] != '\0')
+	{
+		if (n < length)
+		{
+			dest[Q] = src[R] - n;
+			Q++;
+			R++;
+		}
+		else if (n > length)
+		{
+			dest[Q] = src[R];
+			Q++;
+			R++;
+		}
+		dest[Q] = '\0';
+		return (dest);
+	}
 }
