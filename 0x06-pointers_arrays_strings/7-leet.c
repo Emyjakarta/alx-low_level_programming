b95e738ed13895679b20609d56502a3581dab96f
Emyjakarta <ukonud4@gmail.com>
2023-09-21 16:54:21 +0100

    Encode

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -2,11 +2,7 @@
 /**
  * leet-function that encodes a string into 1337
  * @Q: string to be updated
- * You can only use one if in your code
- * You can only use two loops in your code
- * You are not allowed to use switch
- * You are not allowed to use any ternary operation
- * Return:The updated string
+ i* Return:The updated string
  */
 char *leet(char *Q)
 {
@@ -14,17 +10,20 @@ char *leet(char *Q)
 	char *alpha = "aAeEoOtTlL";
 	char *update = "4433007711";
 
-	R = 0, S = 0;
+	R = 0;
 
 	while (Q[R] != '\0')
 	{
+		S = 0;
 		while (alpha[S] != '\0')
 		{
 			if (Q[R] == alpha[S])
+			{
 				Q[R] = update[S];
+				break;
+			}
 			S++;
 		}
-		S = 0;
 		R++;
 	}
 	return (Q);
