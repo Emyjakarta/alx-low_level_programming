ee94df49d013afce82c5f9459454d32085532e3f
Emyjakarta <ukonud4@gmail.com>
2023-09-21 12:25:45 +0100

    Add one line

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * leet-function that encodes a string into 1337
+ * @Q: string to be updated
+ * You can only use one if in your code
+ * You can only use two loops in your code
+ * You are not allowed to use switch
+ * You are not allowed to use any ternary operation
+ * Return: The updated string
+ *
