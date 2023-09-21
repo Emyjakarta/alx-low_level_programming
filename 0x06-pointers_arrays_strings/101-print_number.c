1991f45260d33df4d8286171cdf27044515725a0
Emyjakarta <ukonud4@gmail.com>
2023-09-21 19:10:39 +0100

    Print Integer

---
+++ 0x06-pointers_arrays_strings/101-print_number.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_number-a function that prints an integer.
+ * @n: integer to be printed
+ * You can only use _putchar function to print
+ * You are not allowed to use long
+ * You are not allowed to use arrays or pointers
+ * You are not allowed to hard-code special values
+ * Return:Integer
+ */
+void print_number(int n)
+{
+	unsigned int Q;
+
+	if (n < 0)
+	{
+		Q = -n;
+		_putchar('-');
+	}
+	else
+	{
+		Q = n;
+	}
+	if (Q / 10)
+		print_number(Q / 10);
+	_putchar((Q % 10) + 48);
+}
