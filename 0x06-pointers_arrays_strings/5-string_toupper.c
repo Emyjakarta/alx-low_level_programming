4e965b37175acdb973b83d6e58b6680b0e1c222a
Emyjakarta <ukonud4@gmail.com>
2023-09-21 08:37:41 +0100

    lower - upper

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+/**
+ * string_toupper-a function that changes all lowercase
+ * letters of a string to uppercase
+ * Return:subtract 32 from the ASCII value of the letter
+ */
+char *string_toupper(char *Q)
+	int R;
+
+	for (R = 0; Q[R] != '\0'; R++)
+	{
+		if (Q[R] > 96 && Q[R] < 123)
+		{
+			Q[R] -= 32;
+		}
+	}
+	return (Q);
+
+
