732f99fe230feb24dbf73a0a6081352493aa2c5f
Emyjakarta <ukonud4@gmail.com>
2023-09-29 23:00:26 +0100

    Update square root function

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,25 +1,28 @@
 #include "main.h"
 /**
- * _sqrt-a function that returns the
- * @Q: number that defines the square root
- * @R: number that compares Q
- * Return: square root of number or -1
+ * _sqrt-a function that calculates the square root
+ * of a number recursively
+ * @Q: number for which we want to find the square root
+ * @R: number that compares Q. It starts at 1 and is used in
+ * the recursive calculation
+ * Return: square root of number or -1 if it's not a
+ * perfect square
  */
 int _sqrt(int Q, int R)
 {
-	int sqrt_rec = R * R;
+	int sqrt_rec = R * R; /*calculate the square of current R*/
 
-	if (sqrt_rec == Q)
+	if (sqrt_rec == Q) /*if R squared == Q, return R as square root*/
 		return (R);
-	if (sqrt_rec < Q)
+	if (sqrt_rec < Q) /*if R squared < Q, recall _sqrt*/
 		return (_sqrt(Q, R + 1));
-	else
+	else /*if R squared > Q, Q isn't a perfect square, return -1*/
 		return (-1);
 }
 /**
- * _sqrt_recursion-a function that returns the
- * natural square root of a number.
- * @n: number
+ * _sqrt_recursion-a wrapper function that returns the
+ * natural square root of a number. it calls _sqrt
+ * @n: number for which we want to find the square root
  * Return: square root of number
  */
 int _sqrt_recursion(int n)
