9984fb8d4415fc1fba9fba7a25735398a2952c0c
Emyjakarta <ukonud4@gmail.com>
2023-10-02 15:57:04 +0100

    Print Minimum Number of Coins

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -17,23 +17,23 @@
 */
 int main(int argc, char **argv)
 {
+	int Q;
+	int coins[] = {25, 10, 5, 2, 1};
+	int coins_number = sizeof(coins) / sizeof(coins[0]);
+	int count_coins = 0, i = 0;
+
 	if (argc != 2)
 	{
 		printf("Error\n");
 		return (1);
 	}
-	int Q = atoi(argv[1]);
+	Q = atoi(argv[1]);
 
 	if (Q < 0)
 	{
 		printf("0\n");
 		return (0);
 	}
-	int *coins = {25, 10, 5, 2, 1, '\0'};
-	int coins_number = sizeof(coins) / sizeof(coins[0]);
-
-	int count_coins = 0, i = 0;
-
 	while (coins_number > i)
 	{
 		while (coins[i] <= Q)
