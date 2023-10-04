81256cbf99d11ea1414941d1b05cac98edbb5dcd
Emyjakarta <ukonud4@gmail.com>
2023-10-05 00:41:07 +0100

    Concatenate all arguments

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -43,7 +43,10 @@ char *argstostr(int ac, char **av)
 			new_string[post++] = av[Q][length_av];
 			length_av++;
 		}
-		new_string[post++] = '\n';
+		if (ac - 1 > Q)
+		{
+			new_string[post++] = '\n';
+		}
 		Q++;
 	}
 	new_string[post] = '\0';
---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * strtow-a function that splits a string into words
+ * The function returns a pointer to an array of strings (words)
+ * @str: string
+ * Each element of this array should contain a single word, null-terminated
+ * The last element of the returned array should be NULL
+ * Words are separated by spaces
+ * Returns NULL if str == NULL or str == ""
+ * If your function fails, it should return NULL
+ * Return:pointer to an array of strings
+ */
+char **strtow(char *str)
+{
+	
+}
