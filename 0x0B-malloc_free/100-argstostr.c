00829e62da0474bebebf2820510949df513767a0
Emyjakarta <ukonud4@gmail.com>
2023-10-04 16:24:05 +0100

    Concatenate All Strings

--- 0x0B-malloc_free/100-argstostr.c
+++ 0x0B-malloc_free/100-argstostr.c
@@ -12,7 +12,7 @@
  */
 char *argstostr(int ac, char **av)
 {
-	int Q = 0, overall_length = ac - 1, length_av = 0, post = 0;
+	int Q = 0, overall_length = 0, length_av = 0, post = 0;
 	char *new_string;
 
 	if (ac == 0 || av == NULL)
@@ -20,14 +20,14 @@ char *argstostr(int ac, char **av)
 	while (ac > Q)
 	{
 		length_av = 0;
-		while (av[Q][length_av])
+		while (av[Q][length_av] != '\0')
 		{
 			length_av++;
-			overall_length++;
 		}
+		overall_length += length_av + 1;
 		Q++;
 	}
-	new_string = (char *)malloc(sizeof(char) * (overall_length + 1));
+	new_string = malloc(overall_length + 1);
 	if (new_string == NULL)
 	{
 		return (NULL);
@@ -37,12 +37,14 @@ char *argstostr(int ac, char **av)
 	while (ac > Q)
 	{
 		length_av = 0;
-		while (av[Q][length_av])
+		while (av[Q][length_av] != '\0')
 		{
-			new_string[post++] = av[Q][length_av++];
+			new_string[post++] = av[Q][length_av];
+			length_av++;
 		}
 		new_string[post++] = '\n';
+		Q++;
 	}
-	new_string[overall_length] = '\0';
+	new_string[post - 1] = '\0';
 	return (new_string);
 }
