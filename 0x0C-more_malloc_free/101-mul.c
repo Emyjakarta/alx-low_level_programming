75f90dd9172be87f4a1d99deb5f27b7b2d2f3f92
Emyjakarta <ukonud4@gmail.com>
2023-10-09 00:59:29 +0100

    Multiply two positive numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,8 +1,6 @@
 #include "main.h"
-#include <stddef.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <string.h>
 /**
  * mult-multiply two positive numbers
  * @num1: first positive number
@@ -56,7 +54,6 @@ int _atoi(char *str)
  */
 int main(int argc, char *argv[])
 {
-	char *num1s, *num2s;
 	int num1, num2, result;
 
 	if (argc != 3)
@@ -64,27 +61,20 @@ int main(int argc, char *argv[])
 		printf("Error\n");
 		exit(98);
 	}
-	num1s = calloc(strlen(argv[1]) + 1, sizeof(char));
-	num2s = calloc(strlen(argv[2]) + 1, sizeof(char));
-	if (num1s == NULL || num2s == NULL)
-	{
-		printf("Allocation of memory failed.\n");
-		return (1);
-	}
-	strcpy(num1s, argv[1]);
-	strcpy(num2s, argv[2]);
-	if (!_isnumber(num1s) || !_isnumber(num2s))
+	if (!_isnumber(argv[1]) || !_isnumber(argv[2]))
 	{
 		printf("Error\n");
-		free(num1s);
-		free(num2s);
 		exit(98);
 	}
-	num1 = _atoi(num1s);
-	num2 = _atoi(num2s);
+	num1 = _atoi(argv[1]);
+	num2 = _atoi(argv[2]);
+	if ((num1 > 0 && num2 > INT_MAX / num1) ||
+			(num1 < 0 && num2 < INT_MAX / num1))
+	{
+		printf("Error\n");
+		exit(98);
+	}
 	result = mult(num1, num2);
 	printf("%i\n", result);
-	free(num1s);
-	free(num2s);
 	return (0);
 }
