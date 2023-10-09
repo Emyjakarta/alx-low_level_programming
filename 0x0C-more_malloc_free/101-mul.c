aa6abe0678680a7a562410b4eb8e47e2713c7bd9
Emyjakarta <ukonud4@gmail.com>
2023-10-09 12:43:47 +0100

    Multiply 2 positive integers including very huge numbers

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -14,16 +14,14 @@ char *multstr(char num1[], char num2[])
 	int lent1 = strlen(num1), i, j;
 	int lent2 = strlen(num2), prod, sum, k;
 	int _resultsize = lent1 + lent2;
-	int *_result = malloc(sizeof(int) * _resultsize);
+	int *_result = calloc(_resultsize, sizeof(int));
 	char *_resultstring;
 
-	for (i = 0; i < _resultsize; i++)
-		_result[i] = 0;
 	for (i = lent1 - 1; i >= 0; i--)
 	{
 		for (j = lent2 - 1; j >= 0; j--)
 		{
-			prod = (num1[i] - '0') * (num2[i] - '0');
+			prod = (num1[i] - '0') * (num2[j] - '0');
 			sum = prod + _result[i + j + 1];
 			_result[i + j + 1] = sum % 10;
 			_result[i + j] += sum / 10;
@@ -70,21 +68,14 @@ int _ispositivenumber(char *str)
  */
 int main(int argc, char *argv[])
 {
-	char *num1str, *num2str, *result;
+	char *result;
 
-	if (argc != 3)
+	if (argc != 3 || !_ispositivenumber(argv[1]) || !_ispositivenumber(argv[2]))
 	{
 		printf("Error\n");
 		exit(98);
 	}
-	num1str = argv[1];
-	num2str = argv[2];
-	if (!_ispositivenumber(num1str) || !_ispositivenumber(num2str))
-	{
-		printf("Error\n");
-		exit(98);
-	}
-	result = multstr(num1str, num2str);
+	result = multstr(argv[1], argv[2]);
 	printf("%s\n", result);
 	free(result);
 	return (0);
