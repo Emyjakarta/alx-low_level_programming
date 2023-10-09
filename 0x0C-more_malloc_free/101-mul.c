98341af075c010b474dd4a5660c663d2f5adedb5
Emyjakarta <ukonud4@gmail.com>
2023-10-09 09:43:02 +0100

    MULTIPLY 2 POSITIVE NUMBERS

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -2,30 +2,32 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <limits.h>
+#include <ctype.h>
 /**
  * mult-multiply two positive numbers
  * @num1: first positive number
  * @num2: second positive number
  * Return: num1 * num2
  */
-int mult(int num1, int num2)
+long long int mult(int num1, int num2)
 {
-	return (num1 * num2);
+	return ((long long int)num1 * num2);
 }
 /**
  * _isnumber-check if string is a number
  * @str: string
- * Return:0
+ * Return:1 if number, 0 otherwise
  */
 int _isnumber(char *str)
 {
 	int i = 0;
 
-	while (str[i])
+	while (str[i] != '\0')
 	{
 		if (str[i] < '0' || str[i] > '9')
 		{
-			return (0);
+			printf("Error\n");
+			return (98);
 		}
 		i++;
 	}
@@ -48,14 +50,15 @@ int _atoi(char *str)
 	return (result);
 }
 /**
- * main-takes two arguments
+ * main-takes two arguments and multiplies them
  * @argc: number of arguments
  * @argv: argument vector
  * Return:Always 0 (Success)
  */
 int main(int argc, char *argv[])
 {
-	int num1, num2, result;
+	int num1, num2; 
+	long long int result;
 
 	if (argc != 3)
 	{
@@ -69,13 +72,12 @@ int main(int argc, char *argv[])
 	}
 	num1 = _atoi(argv[1]);
 	num2 = _atoi(argv[2]);
-	if ((num1 > 0 && num2 > INT_MAX / num1) ||
-			(num1 < 0 && num2 < INT_MAX / num1))
+	if (num1 > 0 && num2 > INT_MAX / num1)
 	{
 		printf("Error\n");
 		exit(98);
 	}
 	result = mult(num1, num2);
-	printf("%i\n", result);
+	printf("%lld\n", result);
 	return (0);
 }
