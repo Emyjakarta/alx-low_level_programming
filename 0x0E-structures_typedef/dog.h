ddf3d6d79e6e65a858e54cde6cc28a96518b2323
Emyjakarta <ukonud4@gmail.com>
2023-10-10 13:14:16 +0100

    User defined data type

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog-User defined data type
+ * @name: name
+ * @age: age
+ * @owner: owner
+ */
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+};
+
+#endif /* DOG_H */
