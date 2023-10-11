48c1e562f8428584a117533120e3e402e51ab208
Emyjakarta <ukonud4@gmail.com>
2023-10-11 18:20:15 +0100

    Basic Calculator

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -19,7 +19,7 @@ int (*get_op_func(char *s))(int, int)
 	};
 	for (Q = 0; ops[Q].op; Q++)
 	{
-		if (*(ops[Q].op) == *s && *(s + 1) == '\0')
+		if (strcmp(ops[Q].op), s) == 0)
 			return (ops[Q].f);
 	}
 	return (NULL);
