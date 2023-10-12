4a9cfb5c9019e536ce65f66e5347d1eaa05e76af
Emyjakarta <ukonud4@gmail.com>
2023-10-12 14:51:14 +0100

    Print Anything

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,55 @@
+#include "variadic_functions.h"
+/**
+ * print_all-a function that prints anything
+ * @format: is a list of types of arguments passed to the function
+ * c: char
+ * i: integer
+ * f: float
+ * s: char * (if the string is NULL, print (nil) instead
+ * any other char should be ignored
+ * You are not allowed to use for, goto, ternary operator, else, do ... while
+ * You can use a maximum of
+ * 2 while loops
+ * 2 if
+ * You can declare a maximum of 9 variables
+ * You are allowed to use printf
+ * Print a new line at the end of your function
+ * Return:void
+ */
+void print_all(const char *const format, ...)
+{
+	int count;
+	char *string, *separator;
+	va_list ptr;
+
+	va_start(ptr, format);
+	separator = "";
+	count = 0;
+	while (format && format[count])
+	{
+		if (format[count] == 'c')
+		{
+			printf("%s%c", separator, va_arg(ptr, int));
+		}
+		else if (format[count] == 'i')
+		{
+			printf("%s%i", separator, va_arg(ptr, int));
+		}
+		else if (format[count] == 'f')
+		{
+			printf("%s%f", separator, va_arg(ptr, double));
+		}
+		else if (format[count] == 's')
+		{
+			string = va_arg(ptr, char *);
+			if (string == NULL)
+				printf("(nil)");
+			else
+				printf("%s%s", separator, string);
+		}
+		separator = ", ";
+		count++;
+	}
+	printf("\n");
+	va_end(ptr);
+}
