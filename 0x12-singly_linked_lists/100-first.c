d5f831d0890735b4533c259121643f2f6ad3483d
Emyjakarta <ukonud4@gmail.com>
2023-10-19 15:04:25 +0100

    a function that prints its content before the contents of the main function

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,11 @@
+#include "lists.h"
+/**
+ * _first-function that prints a text before the contents of the main function
+ * Return:void
+ */
+void __attribute__((constructor)) _first()
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
