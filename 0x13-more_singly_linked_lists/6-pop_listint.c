009afd5f5376a15a127466c9cf626e33be65ef68
Emyjakarta <ukonud4@gmail.com>
2023-10-23 18:14:18 +0100

    Correction

--- 0x13-more_singly_linked_lists/6-pop_listint.c
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -1,8 +1,9 @@
 #include "lists.h"
 /**
- * pop_listint-a function that deletes the head node of a 
+ * pop_listint-a function that deletes the head node of a
  * listint_t linked list, and returns the head node’s data (n)
  * if the linked list is empty return 0
+ * @head: pointer to a pointer to the head of the linked list
  * Return:head node's data (n) or 0 if empty
  */
 int pop_listint(listint_t **head)
--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -1,6 +1,6 @@
 #include "lists.h"
 /**
- * get_nodeint_at_index-a function that returns the 
+ * get_nodeint_at_index-a function that returns the
  * nth node of a listint_t linked list
  * @head: pointer to the head of the linked list
  * @index: index of the node, starting at 0
@@ -12,7 +12,7 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 
 	while (head != NULL)
 	{
-		if (Q == index)
+		if (index == Q)
 		{
 			return (head);
 		}
