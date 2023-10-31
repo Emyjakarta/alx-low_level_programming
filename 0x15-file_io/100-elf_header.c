ce3b67b983effcade5da30457d9df3f70804dc32
Emyjakarta <ukonud4@gmail.com>
2023-10-31 11:40:23 +0100

    Solution to advanced task

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -100,7 +100,7 @@ void show_data(unsigned char *e_ident)
 		printf("2's complement, big endian\n");
 		break;
 	default:
-		printf("<unknown: %x>\n", e_ident[EI_CLASS]);
+		printf("<unknown: %x>\n", e_ident[EI_DATA]);
 	}
 }
 /**
