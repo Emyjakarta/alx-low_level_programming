0d6c4bfdb10e6fbe4ed256528e08606c09d5ecaf
Emyjakarta <ukonud4@gmail.com>
2023-12-22 11:42:28 +0100

    Cleared betty errors

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -12,10 +12,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	unsigned long int idx;
 
 	if (ht == NULL || key == NULL || *key == '\0') /* check validity of inputs */
-	{
 		return (0);
-	}
-	idx = key_index((const unsigned char *)key, ht->size); /* get index using key */
+	/* get index using key */
+	idx = key_index((const unsigned char *)key, ht->size);
 	/* check if key already exists, update value if so*/
 	tmp = ht->array[idx];
 	while (tmp != NULL)
@@ -25,18 +24,14 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			free(tmp->value); /* free the old value */
 			tmp->value = strdup(value); /*duplicate the new value*/
 			if (tmp->value == NULL)
-			{
 				return (0);
-			}
 			return (1);
 		}
 		tmp = tmp->next;
 	}
 	new = malloc(sizeof(hash_node_t));
 	if (new == NULL)
-	{
 		return (0);
-	}
 	new->key = strdup(key); /*duplicate the key*/
 	if (new->key == NULL)
 	{
@@ -49,8 +44,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		free(new->key);
 		free(new);
 		return (0);
-	}
-	/*Add new node at the beginning in case of collision*/
+	} /*Add new node at the beginning in case of collision*/
 	new->next = ht->array[idx];
 	ht->array[idx] = new;
 	return (1);
