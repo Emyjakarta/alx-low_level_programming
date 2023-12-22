9724a067ed491805715f38840f33358ad6513411
Emyjakarta <ukonud4@gmail.com>
2023-12-22 11:34:35 +0100

    function that adds an element to the hash table

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,57 @@
+#include "hash_tables.h"
+/**
+ * hash_table_set-adds an element to the hash table
+ * @ht: hash table to add the key-value pair
+ * @key: the key(string)
+ * @value: the value associated with the key
+ * Return: 1 on success or 0
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	hash_node_t *new = NULL, *tmp = NULL;
+	unsigned long int idx;
+
+	if (ht == NULL || key == NULL || *key == '\0') /* check validity of inputs */
+	{
+		return (0);
+	}
+	idx = key_index((const unsigned char *)key, ht->size); /* get index using key */
+	/* check if key already exists, update value if so*/
+	tmp = ht->array[idx];
+	while (tmp != NULL)
+	{
+		if (strcmp(tmp->key, key) == 0)
+		{
+			free(tmp->value); /* free the old value */
+			tmp->value = strdup(value); /*duplicate the new value*/
+			if (tmp->value == NULL)
+			{
+				return (0);
+			}
+			return (1);
+		}
+		tmp = tmp->next;
+	}
+	new = malloc(sizeof(hash_node_t));
+	if (new == NULL)
+	{
+		return (0);
+	}
+	new->key = strdup(key); /*duplicate the key*/
+	if (new->key == NULL)
+	{
+		free(new);
+		return (0);
+	}
+	new->value = strdup(value); /*duplicate the value*/
+	if (new->value == NULL)
+	{
+		free(new->key);
+		free(new);
+		return (0);
+	}
+	/*Add new node at the beginning in case of collision*/
+	new->next = ht->array[idx];
+	ht->array[idx] = new;
+	return (1);
+}
