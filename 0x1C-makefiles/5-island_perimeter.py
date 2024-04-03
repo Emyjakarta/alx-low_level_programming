78c4f8a115878401a1703b30717033e1c3259e57
Emyjakarta <ukonud4@gmail.com>
2024-04-03 10:16:00 +0100

    a function def island_perimeter(grid): that returns the perimeter of the island described in grid

---
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""Calculate the perimeter of an island in a grid."""
+
+
+def number_water_neighbors(grid, i, j):
+    """Return the number of water neighbors a cell has in a grid."""
+
+    number = 0
+
+    if i <= 0 or not grid[i - 1][j]:
+        number += 1
+    if j <= 0 or not grid[i][j - 1]:
+        number += 1
+    if j >= len(grid[i]) - 1 or not grid[i][j + 1]:
+        number += 1
+    if i >= len(grid) - 1 or not grid[i + 1][j]:
+        number += 1
+
+    return number
+
+
+def island_perimeter(grid):
+    """Returns the perimeter of the island in grid."""
+
+    perimeter = 0
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            if grid[i][j]:
+                perimeter += number_water_neighbors(grid, i, j)
+
+    return perimeter
